4dd2f39dbb8af8a958f5782e51962179438f6d9d
Ignacio Massa <nachomassacese@gmail.com>
2020-05-19 18:40:34 -0300

    move private information into dev.js

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://torquil23:paramongo2020@cluster0-mwrr6.gcp.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true }).then(() => {console.log('Db Connected')}).catch(err => console.error(err));
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser')
 
+const config = require('./config/key')
+
+const { User } = require('./models/User');
+
+mongoose.connect(config.mongoURI, {useNewUrlParser: true }).then(() => {console.log('Db Connected')}).catch(err => console.error(err));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
-    res.send('hello world')
-});
+    res.json({"hello" : "fuuuck"})
+})
 
+app.post('/api/users/register', (req, res) => {
+    const user = new User(req.body);
+    user.save((err, userData) => {
+        if(err) return res.json({ success: false, err })
+    });
+    return res.status(200).json({
+        success: true
+    });
+})
 
 
 app.listen(5000);
